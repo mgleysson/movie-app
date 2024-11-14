bbe3d6acba279905ebe0a2bb6b7af2176af5a6c2
Marcos Gleysson <mgleysson.nascimento@edge.ufal.br>
2024-11-14 10:01:59 -0300

    create domain classes and navigation structure

---
+++ app/src/main/java/br/com/movieapp/core/presentation/ui/MainScreen.kt
@@ -0,0 +1,23 @@
+package br.com.movieapp.core.presentation.ui
+
+import android.annotation.SuppressLint
+import androidx.compose.material.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import br.com.movieapp.core.presentation.navigation.BottomNavigationBar
+import br.com.movieapp.core.presentation.navigation.NavigationGraph
+
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
+@Composable
+fun MainScreen(
+    navController: NavHostController
+) {
+    Scaffold (
+        bottomBar = {
+            BottomNavigationBar(navController = navController)
+        },
+        content = {
+            NavigationGraph(navController = navController)
+        }
+    )
+}
