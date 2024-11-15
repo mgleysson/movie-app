0126ec12134c289063a787b3cfa7b926d5415cb0
Marcos Gleysson <mgleysson.nascimento@edge.ufal.br>
2024-11-15 19:52:50 -0300

    implements paging config, data source, repository, use case and DI module

--- app/src/main/java/br/com/movieapp/MainActivity.kt
+++ app/src/main/java/br/com/movieapp/MainActivity.kt
@@ -3,12 +3,7 @@ package br.com.movieapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.rememberNavController
 import br.com.movieapp.core.presentation.ui.MainScreen
@@ -27,5 +22,15 @@ class MainActivity : ComponentActivity() {
                 )
             }
         }
+    }
+}
+
+@Preview
+@Composable
+fun BottomNavigationBarPreview() {
+    MovieAppTheme {
+        MainScreen(
+            navController = rememberNavController()
+        )
     }
 }
